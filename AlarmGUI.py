bbc4815cd7f1bc94a7a2990a4dda54394728339c
Krzysztof <krzysztof211@gmail.com>
2019-01-25 18:28:45 +0100

    database works with flask

--- AlarmGUI.py
+++ AlarmGUI.py
@@ -4,28 +4,54 @@ Created on Thu Jan  3 13:45:24 2019
 
 @author: Krzysztof Pasiewicz
 """
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, g
 import os
 import sqlite3
 
-db = sqlite3.connect('baza.db')
+DATABASE = 'baza.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+#db = sqlite3.connect('baza.db')
+#global cursor
 #cursor = db.cursor()
 #cursor.execute('CREATE TABLE IF NOT EXISTS alarmy(id INT PRIMARY KEY,user TEXT, godzina INT, minuty INT);')
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 #Tutaj musicie zwrócić alarmy dla użytkownika w formacie id|hour|active;
 #Tylko dla aktywnego użytkownika
 #ostatni alarm bez średnika !!
 @app.route("/getAlarms")
 def getAlarms():
-    return "1|12.40|1;2|13.30|0;3|14.30|0;4|15.30|0;5|15.20|0;6|16.30|0;7|17.30|0"
+    global user
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM alarmy WHERE user =  ?;', (user,))
+    alarms = cursor.fetchall()
+    output = ""
+    for alarm in alarms:
+        output = output + str(alarm[0]) + "|" + str(alarm[2]) + "." + str(alarm[3]) + "|0;"
+    output = output[:-1]
+    return output
 
 #usuwanie alarmu, na wejściu dostajecie jego id, jak się powiedzie zwracacie "ok", jak nie to "false";
 @app.route('/deleteAlarm', methods=['GET'])
 def drop_alarm():
     alarmID = request.args.get('alarmID')
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM alarmy WHERE id =  ?;', (alarmID,))
     print(alarmID)
     return "ok"
 
@@ -48,6 +74,22 @@ def activate_alarm():
 @app.route('/')
 def home():
     global user
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS alarmy(id INT PRIMARY KEY,user TEXT, godzina INT, minuty INT);')
+    cursor.execute('SELECT * FROM alarmy;')
+    id_val = cursor.fetchall()
+    print(id_val)
+    """cursor.execute('INSERT INTO alarmy VALUES(?,?,?,?);',(1,'admin', 10, 24))
+    cursor.execute('INSERT INTO alarmy VALUES(?,?,?,?);',(2,'admin', 12, 24))
+    cursor.execute('INSERT INTO alarmy VALUES(?,?,?,?);',(3,'admin', 13, 24))
+    cursor.execute('SELECT * FROM alarmy;')
+    db.commit()
+    id_val = cursor.fetchall()
+    print(id_val)"""
+    cursor.execute('SELECT MAX(id) FROM alarmy;')
+    id_val = cursor.fetchall()
+    print(id_val)
     if not session.get('logged_in'):
         return render_template('newLogin.html')
     else:
@@ -64,11 +106,6 @@ def do_admin_login():
         flash('no user!')
     return home()
 
-
-
-
-
-
 @app.route('/toset')
 def passSet():
     return render_template('Set.html')
@@ -82,17 +119,21 @@ def setAlarm():
     if hour and minute:
         hour = int(hour)
         minute = int(minute)
-        """print(hour)
-        print(type(hour))
-        print(minute)"""
         if hour > 0 and hour < 24 and minute > 0 and minute < 60:
-            print(hour)
-            print(type(hour))
-            print(minute)
-            #cursor.execute('SELECT MAX(id) FROM alarmy;')
-            #id_val = cursor.fetchall()
-            #id_val = id_val + 1
-            #cursor.execute('INSERT INTO alarmy VALUES(?,?,?,?);',(id_val,user, hour, minute))
+#            print(hour)
+#            print(type(hour))
+#            print(minute)
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute('SELECT MAX(id) FROM alarmy;')
+            id_val = cursor.fetchall()
+            print(id_val[0][0])
+            if id_val[0][0] is not None:
+                id_out = id_val[0][0] + 1
+            else:
+                id_out = 1
+            cursor.execute('INSERT INTO alarmy VALUES(?,?,?,?);',(id_out,user, hour, minute))
+            db.commit()
             #set alarm time and add to database here TODO
         else:
             flash('wrong time format!')
--- guziki_do_przetestowania.py
+++ guziki_do_przetestowania.py
@@ -4,13 +4,13 @@ import time as t
 import random
 import sqlite3
 
-#polaczenie z baza
+# polaczenie z baza
 '''db = sqlite3.connect('baza.db')
 cursor = db.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS alarmy(id INT PRIMARY KEY,user TEXT, godzina INT, minuty INT);')
 db.commit()'''
 
-#inicjalizacja
+# inicjalizacja
 GPIO.setup("P8_8", GPIO.IN)
 GPIO.setup("P8_10", GPIO.IN)
 GPIO.setup("P8_12", GPIO.IN)
@@ -21,45 +21,42 @@ GPIO.setup("P8_9", GPIO.OUT)
 GPIO.setup("P8_11", GPIO.OUT)
 GPIO.setup("P8_15", GPIO.OUT)
 
-#wlaczenie glosnika
-PWM.start("P8_13",25,1000)
+# wlaczenie glosnika
+PWM.start("P8_13", 25, 1000)
 
-#zapalanie diod i gaszenie po wcisnieciu guzika
+# zapalanie diod i gaszenie po wcisnieciu guzika
 for i in range(5):
-    rand=random.randrange(1,5,1)
-    button = 0
-    if random==1:
+    rand = random.randrange(1, 5, 1)
+    button = 1
+    if rand == 1:
         GPIO.output("P8_7", GPIO.HIGH)
-        while button==0:
-            button=GPIO.input("P8_8")
-        while button==1:
-            button=GPIO.input("P8_8")
+        while button == 1:
+            button = GPIO.input("P8_8")
+        while button == 0:
+            button = GPIO.input("P8_8")
         GPIO.output("P8_7", GPIO.LOW)
-    elif random==2:
+    elif rand == 2:
         GPIO.output("P8_9", GPIO.HIGH)
-        while button==0:
-            button=GPIO.input("P8_10")
-        while button==1:
-            button=GPIO.input("P8_10")
+        while button == 1:
+            button = GPIO.input("P8_10")
+        while button == 0:
+            button = GPIO.input("P8_10")
         GPIO.output("P8_9", GPIO.LOW)
-    elif random == 3:
+    elif rand == 3:
         GPIO.output("P8_11", GPIO.HIGH)
-        while button==0:
-            button=GPIO.input("P8_12")
-        while button==1:
-            button=GPIO.input("P8_12")
+        while button == 1:
+            button = GPIO.input("P8_12")
+        while button == 0:
+            button = GPIO.input("P8_12")
         GPIO.output("P8_11", GPIO.LOW)
-    elif random==4:
+    elif rand == 4:
         GPIO.output("P8_15", GPIO.HIGH)
-        while button==0:
-            button=GPIO.input("P8_16")
-        while button==1:
-            button=GPIO.input("P8_16")
+        while button == 1:
+            button = GPIO.input("P8_16")
+        while button == 0:
+            button = GPIO.input("P8_16")
         GPIO.output("P8_15", GPIO.LOW)
-    
-    
+print("zakonzono")
 PWM.stop("P8_13")
 PWM.cleanup()
 GPIO.cleanup()
-
-
